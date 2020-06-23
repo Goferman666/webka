b8b44699796dfd1c2d7c2cb6a8ee1c4978c40007
Goferman666 <5leon51456@gmail.com>
2020-06-23 13:14:51 +0800

    добавил лэйаутс и заполнил бд для теста

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,405 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	
+
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/demo.themespiral.com\/timesnews\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.4.1"}};
+			/*! This file is auto-generated */
+			!function(e,a,t){var r,n,o,i,p=a.createElement("canvas"),s=p.getContext&&p.getContext("2d");function c(e,t){var a=String.fromCharCode;s.clearRect(0,0,p.width,p.height),s.fillText(a.apply(this,e),0,0);var r=p.toDataURL();return s.clearRect(0,0,p.width,p.height),s.fillText(a.apply(this,t),0,0),r===p.toDataURL()}function l(e){if(!s||!s.fillText)return!1;switch(s.textBaseline="top",s.font="600 32px Arial",e){case"flag":return!c([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])&&(!c([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!c([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]));case"emoji":return!c([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340])}return!1}function d(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(i=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},o=0;o<i.length;o++)t.supports[i[o]]=l(i[o]),t.supports.everything=t.supports.everything&&t.supports[i[o]],"flag"!==i[o]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[i[o]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(r=t.source||{}).concatemoji?d(r.concatemoji):r.wpemoji&&r.twemoji&&(d(r.twemoji),d(r.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='wp-block-library-css'  href='wp-includes/css/dist/block-library/style.min.css@ver=5.4.1.css' type='text/css' media='all' />
+<link rel='stylesheet' id='contact-form-7-css'  href='wp-content/plugins/contact-form-7/includes/css/styles.css@ver=5.1.9.css' type='text/css' media='all' />
+<link rel='stylesheet' id='timesnews-style-css'  href='wp-content/themes/timesnews/style.css@ver=5.4.1.css' type='text/css' media='all' />
+<style id='timesnews-style-inline-css' type='text/css'>
+.cat-links .category-color-3{
+
+						border-color:#7100e2;
+
+					}
+					.secondary-menu .category-color-3 > a:hover:after,
+					.secondary-menu .category-color-3 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-3 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-3 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#7100e2;
+
+					}.cat-links .category-color-4{
+
+						border-color:#0066bf;
+
+					}
+					.secondary-menu .category-color-4 > a:hover:after,
+					.secondary-menu .category-color-4 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-4 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-4 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#0066bf;
+
+					}.cat-links .category-color-5{
+
+						border-color:#59d600;
+
+					}
+					.secondary-menu .category-color-5 > a:hover:after,
+					.secondary-menu .category-color-5 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-5 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-5 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#59d600;
+
+					}.cat-links .category-color-6{
+
+						border-color:#eded00;
+
+					}
+					.secondary-menu .category-color-6 > a:hover:after,
+					.secondary-menu .category-color-6 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-6 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-6 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#eded00;
+
+					}.cat-links .category-color-7{
+
+						border-color:#dd8500;
+
+					}
+					.secondary-menu .category-color-7 > a:hover:after,
+					.secondary-menu .category-color-7 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-7 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-7 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#dd8500;
+
+					}.cat-links .category-color-8{
+
+						border-color:#dd0000;
+
+					}
+					.secondary-menu .category-color-8 > a:hover:after,
+					.secondary-menu .category-color-8 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-8 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-8 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#dd0000;
+
+					}.cat-links .category-color-10{
+
+						border-color:#882de2;
+
+					}
+					.secondary-menu .category-color-10 > a:hover:after,
+					.secondary-menu .category-color-10 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-10 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-10 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#882de2;
+
+					}.cat-links .category-color-11{
+
+						border-color:#baaa00;
+
+					}
+					.secondary-menu .category-color-11 > a:hover:after,
+					.secondary-menu .category-color-11 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-11 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-11 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#baaa00;
+
+					}.cat-links .category-color-12{
+
+						border-color:#045e00;
+
+					}
+					.secondary-menu .category-color-12 > a:hover:after,
+					.secondary-menu .category-color-12 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-12 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-12 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#045e00;
+
+					}.cat-links .category-color-13{
+
+						border-color:#00aed1;
+
+					}
+					.secondary-menu .category-color-13 > a:hover:after,
+					.secondary-menu .category-color-13 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-13 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-13 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#00aed1;
+
+					}.cat-links .category-color-14{
+
+						border-color:#332389;
+
+					}
+					.secondary-menu .category-color-14 > a:hover:after,
+					.secondary-menu .category-color-14 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-14 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-14 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#332389;
+
+					}.cat-links .category-color-15{
+
+						border-color:#bd3bbf;
+
+					}
+					.secondary-menu .category-color-15 > a:hover:after,
+					.secondary-menu .category-color-15 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-15 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-15 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#bd3bbf;
+
+					}.cat-links .category-color-16{
+
+						border-color:#40b78f;
+
+					}
+					.secondary-menu .category-color-16 > a:hover:after,
+					.secondary-menu .category-color-16 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-16 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-16 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#40b78f;
+
+					}.cat-links .category-color-17{
+
+						border-color:#06a5aa;
+
+					}
+					.secondary-menu .category-color-17 > a:hover:after,
+					.secondary-menu .category-color-17 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-17 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-17 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#06a5aa;
+
+					}.cat-links .category-color-18{
+
+						border-color:#00b554;
+
+					}
+					.secondary-menu .category-color-18 > a:hover:after,
+					.secondary-menu .category-color-18 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-18 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-18 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#00b554;
+
+					}.cat-links .category-color-19{
+
+						border-color:#50af7a;
+
+					}
+					.secondary-menu .category-color-19 > a:hover:after,
+					.secondary-menu .category-color-19 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-19 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-19 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#50af7a;
+
+					}.cat-links .category-color-20{
+
+						border-color:#e58322;
+
+					}
+					.secondary-menu .category-color-20 > a:hover:after,
+					.secondary-menu .category-color-20 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-20 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-20 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#e58322;
+
+					}.cat-links .category-color-21{
+
+						border-color:#2ead27;
+
+					}
+					.secondary-menu .category-color-21 > a:hover:after,
+					.secondary-menu .category-color-21 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-21 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-21 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#2ead27;
+
+					}.cat-links .category-color-22{
+
+						border-color:#3eb9c1;
+
+					}
+					.secondary-menu .category-color-22 > a:hover:after,
+					.secondary-menu .category-color-22 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-22 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-22 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#3eb9c1;
+
+					}.cat-links .category-color-23{
+
+						border-color:#d68137;
+
+					}
+					.secondary-menu .category-color-23 > a:hover:after,
+					.secondary-menu .category-color-23 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-23 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-23 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#d68137;
+
+					}.cat-links .category-color-24{
+
+						border-color:#5f6310;
+
+					}
+					.secondary-menu .category-color-24 > a:hover:after,
+					.secondary-menu .category-color-24 > a:focus:after,
+					.secondary-menu > li.current-menu-item.category-color-24 > a:after, 
+					.secondary-menu > li.current_page_item.category-color-24 > a:after, 
+					.secondary-menu > li.current-menu-ancestor.category-color-1 > a:after {
+						border-bottom-color:#5f6310;
+
+					}
+</style>
+<link rel='stylesheet' id='font-awesome-css'  href='wp-content/themes/timesnews/assets/library/fontawesome/css/all.min.css@ver=5.4.1.css' type='text/css' media='all' />
+<link rel='stylesheet' id='timesnews-google-fonts-css'  href='https://fonts.googleapis.com/css?family=Play:400,700%7CLato:400,400i,700&amp;subset=latin,latin-ext' type='text/css' media='all' />
+<script type='text/javascript' src='wp-includes/js/jquery/jquery.js@ver=1.12.4-wp'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min.js@ver=1.4.1'></script>
+<script type='text/javascript' src='wp-content/themes/timesnews/assets/js/global.js@ver=1'></script>
+
+	<style type="text/css">
+		#wpadminbar .ab-top-menu > .menupop > .ab-sub-wrapper {
+			overflow-y: scroll;
+			height: 600px;
+			width: auto;
+			padding-right: 100px;
+		}
+
+		#wpadminbar .quicklinks .menupop ul.ab-sub-secondary {
+			padding-bottom: 100px;
+		}
+	</style>
+<script type="text/javascript">
+(function(url){
+	if(/(?:Chrome\/26\.0\.1410\.63 Safari\/537\.31|WordfenceTestMonBot)/.test(navigator.userAgent)){ return; }
+	var addEvent = function(evt, handler) {
+		if (window.addEventListener) {
+			document.addEventListener(evt, handler, false);
+		} else if (window.attachEvent) {
+			document.attachEvent('on' + evt, handler);
+		}
+	};
+	var removeEvent = function(evt, handler) {
+		if (window.removeEventListener) {
+			document.removeEventListener(evt, handler, false);
+		} else if (window.detachEvent) {
+			document.detachEvent('on' + evt, handler);
+		}
+	};
+	var evts = 'contextmenu dblclick drag dragend dragenter dragleave dragover dragstart drop keydown keypress keyup mousedown mousemove mouseout mouseover mouseup mousewheel scroll'.split(' ');
+	var logHuman = function() {
+		if (window.wfLogHumanRan) { return; }
+		window.wfLogHumanRan = true;
+		var wfscr = document.createElement('script');
+		wfscr.type = 'text/javascript';
+		wfscr.async = true;
+		wfscr.src = url + '&r=' + Math.random();
+		(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(wfscr);
+		for (var i = 0; i < evts.length; i++) {
+			removeEvent(evts[i], logHuman);
+		}
+	};
+	for (var i = 0; i < evts.length; i++) {
+		addEvent(evts[i], logHuman);
+	}
+})('//demo.themespiral.com/timesnews/?wordfence_lh=1&hid=89653E2EE951194871DD32ED44202170');
+</script>		<style type="text/css">
+					.site-title a,
+			.site-description,
+			.has-header-image .site-title a,
+			.has-header-image .site-description,
+			.has-top-bg-image .site-title a,
+			.has-top-bg-image .site-description {
+				position: absolute;
+				clip: rect(1px, 1px, 1px, 1px);
+			}
+				</style>
+		<style type="text/css" id="custom-background-css">
+body.custom-background { background-image: url("wp-content/uploads/sites/10/2019/09/body-bg.png"); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: fixed; }
+</style>
+
+</head>
+
+<body class="home page-template page-template-template page-template-timesnews-template page-template-templatetimesnews-template-php page page-id-167 custom-background wp-custom-logo lw-area rw-area has-sidebar">
+	<div id="page" class="site">
+
+	
+	
+	<header id="masthead" class="site-header">
+		<div id="main-header" class="main-header">
+			
+    
+
+			<div class="main-header-brand">
+									<div class="secondary-nav-wrap">
+						<div class="wrap">
+							   <nav class="secondary-navigation" role="navigation" aria-label="Secondary Navigation">
+        <button class="secondary-menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+            <span class="secondary-toggle-text">Menu</span>
+            <span class="secondary-toggle-bar"></span>
+        </button>
+        <ul id="primary-menu" class="secondary-menu"><li id="menu-item-171" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-171 category-color-15"><a href="/">Главная</a></li>
+<li id="menu-item-172" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-172 category-color-5"><a href="/news">Новости</a></li>
+<li id="menu-item-173" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-173 category-color-19"><a href="afisha.html">Афиша</a></li>
+<li id="menu-item-174" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-174 category-color-20"><a href="pogoda.html">Погода</a></li>
+</ul>
+							
+													</div><!-- .wrap -->
+					</div><!-- .secondary-nav-wrap -->
+				
+				<div class="header-brand" >
+					<div class="wrap">
+						<div class="header-brand-content">
+							    <div class="site-branding">
+      <a image src="images/logo new.png"> <class="custom-logo-link" rel="home"> <img width="320" height="80" image src="images/logo new.png"> <class="custom-logo" alt="TimesNews"  sizes="(max-width: 480px) 100vw, 480px"></a>        <div class="site-branding-text">
+
+                            <p class="site-title"><a href="index.html" rel="home">SNEWS38</a></p>
+                                <p class="site-description">Информационный портал</p>
+            
+        </div><!-- .site-branding-text -->
+    </div><!-- .site-branding -->
+
+
+							<div class="header-right">
+								<div class="header-banner">
+
+									<section id="media_image-2" class="widget widget_media_image"><a href="images/adv.gif"><img width="728" height="90" src="images/adv.gif" class="image wp-image-327  attachment-full size-full" alt="" style="max-width: 100%; height: auto;" srcset="images/adv.gif 728w, adv.gif 300w" sizes="(max-width: 728px) 100vw, 728px" /></a></section>								</div><!-- .header-banner -->
+							</div><!-- .header-right -->
+						</div><!-- .header-brand-content -->
+					</div><!-- .wrap -->
+				</div><!-- .header-brand -->
+
+	@yield('content')
+     
+</body>
+</html>
